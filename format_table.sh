f7c090a0eff45d71432d35dd5e0dcc179ee49848
f2l5y <163734677+f2l5y@users.noreply.github.com>
2024-03-27 23:15:55 +0100

    Update format_table.sh

--- format_table.sh
+++ format_table.sh
@@ -1,19 +1,18 @@
-  1 #!bin/bash
-  2 
-  3 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
-  4 
-  5 #Capitalize first letter  of the Symbol column
-  6  echo "$($PSQL "SELECT symbol FROM elements;")" | while IFS="" read NAME
-  7  do
-  8        echo $($PSQL "UPDATE elements SET symbol='$(echo "${NAME^}")' WHERE symbol='$    NAME';")
-  9  done
- 10 
- 11 
- 12 #remove trailing zeros from atomic mass
- 13 
- 14 echo "$($PSQL "SELECT atomic_mass  FROM properties;")" | while IFS="" read MASS
- 15 do
- 16   echo "$($PSQL "UPDATE properties SET atomic_mass="$( echo $MASS | sed -E 's/([1-9]+    )0+$/\1/g')" WHERE atomic_mass='$MASS';")"
- 17 
- 18 done
-~                       
+#!bin/bash
+
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+
+#Capitalize first letter  of the Symbol column
+ echo "$($PSQL "SELECT symbol FROM elements;")" | while IFS="" read NAME
+ do
+  	echo $($PSQL "UPDATE elements SET symbol='$(echo "${NAME^}")' WHERE symbol='$NAME';")
+ done
+
+
+#remove trailing zeros from atomic mass
+
+echo "$($PSQL "SELECT atomic_mass  FROM properties;")" | while IFS="" read MASS
+do
+  echo "$($PSQL "UPDATE properties SET atomic_mass="$( echo $MASS | sed -E 's/([1-9]+)0+$/\1/g')" WHERE atomic_mass='$MASS';")"
+
+done
