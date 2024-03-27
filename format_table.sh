98069d403ca0d2cebafa319a6a7b3f55dd291c24
f2l5y <163734677+f2l5y@users.noreply.github.com>
2024-03-27 21:30:17 +0100

    Update format_table.sh

--- format_table.sh
+++ format_table.sh
@@ -3,10 +3,10 @@
   3 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
   4 
   5 #Capitalize first letter  of the Symbol column
-  6 # echo "$($PSQL "SELECT symbol FROM elements;")" | while IFS="" read NAME
-  7 # do
-  8 #       echo $($PSQL "UPDATE elements SET symbol='$(echo "${NAME^}")' WHERE symbol='$    NAME';")
-  9 # done
+  6  echo "$($PSQL "SELECT symbol FROM elements;")" | while IFS="" read NAME
+  7  do
+  8        echo $($PSQL "UPDATE elements SET symbol='$(echo "${NAME^}")' WHERE symbol='$    NAME';")
+  9  done
  10 
  11 
  12 #remove trailing zeros from atomic mass
