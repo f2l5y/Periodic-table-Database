64f68bf7ce16142f94432ab64a3715d40a7cd5b9
f2l5y <163734677+f2l5y@users.noreply.github.com>
2024-03-27 23:18:24 +0100

    Update element.sh

--- element.sh
+++ element.sh
@@ -9,39 +9,38 @@ if [[ ! $1 ]]; then
 	echo Please provide an element as an argument.
 else
 
-# if a digit
-if  [[ $1 =~ ^[0-9]+$ ]]
-then
-	atomic_number=$($PSQL "SELECT atomic_number FROM elements WHERE atomic_number='$1';") 
-
-# if less then three characters (symbol)
-elif [[ ${#1} -le 3 ]];then
-
-atomic_number=$($PSQL "SELECT atomic_number FROM elements WHERE symbol='$1';") 
-
-# if a name
-else
-atomic_number=$($PSQL "SELECT atomic_number FROM elements WHERE name='$1';") 
-fi
-
-
-# if not found
-if [[ -z $atomic_number ]];then
-   echo "I could not find that element in the database."
-
-# if found, start the queries
-else
-
-name=$($PSQL "SELECT name FROM elements WHERE atomic_number=$atomic_number;")
-symbol=$($PSQL "SELECT symbol FROM elements WHERE atomic_number=$atomic_number;")
-TYPE=$($PSQL "SELECT type  FROM properties FULL JOIN elements USING(atomic_number) WHERE atomic_number=$atomic_number;")
-melting_point=$($PSQL "SELECT melting_point_celsius  FROM properties FULL JOIN elements USING(atomic_number) WHERE atomic_number=$atomic_number;")
-boiling_point=$($PSQL "SELECT boiling_point_celsius FROM properties FULL JOIN elements USING(atomic_number) WHERE atomic_number=$atomic_number;")
-mass=$($PSQL "SELECT atomic_mass  FROM properties FULL JOIN elements USING(atomic_number) WHERE atomic_number=$atomic_number;")
-
-
-echo "The element with atomic number $atomic_number is $name ($symbol). It's a $TYPE, with a mass of $mass amu. $name has a melting point of $melting_point celsius and a boiling point of $boiling_point celsius."
-fi
+	# if a digit
+	if  [[ $1 =~ ^[0-9]+$ ]]
+	then
+		atomic_number=$($PSQL "SELECT atomic_number FROM elements WHERE atomic_number='$1';") 
+	
+	# if less then three characters (symbol)
+	elif [[ ${#1} -le 3 ]];then
+	
+	atomic_number=$($PSQL "SELECT atomic_number FROM elements WHERE symbol='$1';") 
+	
+	# if a name
+	else
+	atomic_number=$($PSQL "SELECT atomic_number FROM elements WHERE name='$1';") 
+	fi
+	
+	
+	# if not found
+	if [[ -z $atomic_number ]];then
+	   echo "I could not find that element in the database."
+	
+	# if found, start the queries
+	else
+	
+		NAME=$($PSQL "SELECT name FROM elements WHERE atomic_number=$atomic_number;")
+		SYMBOL=$($PSQL "SELECT symbol FROM elements WHERE atomic_number=$atomic_number;")
+		TYPE=$($PSQL "SELECT type  FROM properties FULL JOIN elements USING(atomic_number) WHERE atomic_number=$atomic_number;")
+		MELTING_P=$($PSQL "SELECT melting_point_celsius  FROM properties FULL JOIN elements USING(atomic_number) WHERE atomic_number=$atomic_number;")
+		BOILING_P=$($PSQL "SELECT boiling_point_celsius FROM properties FULL JOIN elements USING(atomic_number) WHERE atomic_number=$atomic_number;")
+		MASS=$($PSQL "SELECT atomic_mass  FROM properties FULL JOIN elements USING(atomic_number) WHERE atomic_number=$atomic_number;")
+		
+		echo "The element with atomic number $atomic_number is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING_P celsius and a boiling point of $BOILING_P celsius."
+	fi
 fi
 }
 
